afe117adde628494790888b126debd8193d5632a
Kenneth Church <kwchurch@us.ibm.com>
2024-07-02 03:36:36 -0400

    fixed some bugs

--- src/C/GEE.c
+++ src/C/GEE.c
@@ -8,7 +8,7 @@
 
 void usage()
 {
-  fatal("usage: GEE (Graph Embedding Encoder) X0 X1 X2 Y > Z");
+  fatal("usage: GEE (Graph Embedding Encoder): X0 X1 X2 Y Zprev Zerr > Z");
 }
 
 int my_max(int *vec, long n)
@@ -29,6 +29,7 @@ int *bincount(int *vec, long n, long *nres)
   memset(res, 0, sizeof(int) * *nres);
   for(; vec<end;vec++)
     res[*vec]++;
+
   return res;
 }
 
@@ -46,7 +47,14 @@ void normalize(float *Z, long n, long k)
   for( ; Z < Zend; Z += k)
     normalize_row(Z, k);
 }
-    
+
+void print_bincount(FILE *fd, int *nk, long k)
+{
+  int i;
+  for(i=0;i<k;i++)
+    fprintf(fd, "bin[%d] = %d\n", i, nk[i]);
+  fflush(fd);
+}
 
 int main(int ac, char **av)
 {
@@ -54,8 +62,18 @@ int main(int ac, char **av)
   long s;			/* number of edges (X values) */
   long k;			/* number of hidden dimensions */
   long n;			/* number of labels (Y values) */
+  int i;
 
-  if(ac != 5) usage();
+  if(ac != 7) usage();
+
+  for(i=1;i<6;i++)
+    if(!file_exists(av[i])) {
+      fprintf(stderr, "file not found: %s\n", av[i]);
+      fatal("assertion failed");
+    }
+      
+  FILE *err = fopen(av[6], "w");
+
   
   int *X0 = (int *)mmapfile(av[1], &nX0);
   nX0 /= sizeof(int);
@@ -74,42 +92,116 @@ int main(int ac, char **av)
   int *X0end = X0 + s;
   int *nk = bincount(Y, n, &k);
 
+  print_bincount(err, nk, k);
+
   /* float *nk2 = (float *)malloc(sizeof(float) * n); */
   /* if(!nk2) fatal("malloc failed"); */
   /* int i; */
   /* for(i=0;i<n;i++) nk2[i] = nk[Y[i]]; */
 
-  fprintf(stderr, "s (edges) = %ld, n = %ld, k = %ld\n", s, n, k);
-  
-  float *Z = (float *)malloc(sizeof(float) * n * k);
-  if(!Z) fatal("malloc failed");
+  fprintf(err, "s (edges) = %ld, n = %ld, k = %ld\n", s, n, k);
+  fflush(err);
 
-  memset(Z, 0, sizeof(float) * n * k);
+  fprintf(err, "about to read Z from %s\n", av[5]);
+  fflush(err);
 
+  long nZ;
+  float *Z = (float *)readchars(av[5], &nZ);
+  nZ /= sizeof(float);
+
+  fprintf(err, "found %ld floats\n", nZ);
+  fflush(err);
+
+  if(nZ != n * k) {
+    fprintf(err, "nZ = %ld, n = %ld, k = %ld\n", nZ, n, k);
+    fflush(err);
+    fatal("assertion failed");
+  }
+    
+  // float *Z = (float *)malloc(sizeof(float) * n * k);
+  // if(!Z) fatal("malloc failed");
+
+  // memset(Z, 0, sizeof(float) * n * k);
   // fprintf(stderr, "pt1\n");
 
+  long ntick = 1000000;
+  long tick = (X0end - X0)/ntick;
+  int *X0tick = X0 + tick;
+  int *X0base = X0;
+
+  fprintf(err, "tick = %ld, ntick = %ld\n", tick, ntick);
+  fflush(err);
+
   for(;X0 < X0end; X0++,X1++,X2++) {
+
+    // long gap = X0 - X0base;
+    // fprintf(err, "gap = %ld\n", gap);
+    // fflush(err);
+
+    if(X0 >= X0tick) {
+      fprintf(err, "%f done\n", (X0 - X0base)/(double)ntick);
+      fflush(err);
+      X0tick += tick;
+    }
+
     int v_i = *X0;
     int v_j = *X1;
 
-    if(v_i < 0 || v_i >= n) fatal("assertion failed");
-    if(v_j < 0 || v_j >= n) fatal("assertion failed");
+    if(v_i < 0 || v_i >= n) {
+      fprintf(err, "v_i = %d, assertion failed\n", v_i);
+      fflush(err);
+      fatal("assertion failed");
+    }
+
+    if(v_j < 0 || v_j >= n) {
+      fprintf(err, "v_j = %d, assertion failed\n", v_j);
+      fflush(err);
+      fatal("assertion failed");
+    }
 
     int label_i = Y[v_i];
     int label_j = Y[v_j];
-    
-    if(label_i < 0 || label_i >= k) fatal("assertion failed");
-    if(label_j < 0 || label_j >= k) fatal("assertion failed");
+
+    if(label_i < 0 || label_i >= k) {
+      fprintf(err, "label_i = %d, assertion failed\n", label_i);
+      fflush(err);
+      fatal("assertion failed");
+    }
+
+    if(label_j < 0 || label_j >= k) {
+      fprintf(err, "label_j = %d, assertion failed\n", label_j);      
+      fflush(err);
+      fatal("assertion failed");
+    }
     
     // fprintf(stderr, "edge: %d, %d; labels = %d, %d\n", v_i, v_j, label_i, label_j);
 
-    Z[v_i *k + label_j ] += *X2/(double)nk[label_j];
-    if(v_i != v_j)
-      Z[v_j*k + label_i] += *X2/(double)nk[label_i];
+    long o = v_i *k + label_j ;
+    if(o < 0 || o >= nZ) {
+      fprintf(err, "o is out of range: o = %ld, nZ = %ld\n", o, nZ);
+      fflush(err);
+      fatal("assertion failed");
+    }
+
+    Z[o] += *X2/(double)nk[label_j];
+    if(v_i != v_j) {
+      long o2 = v_j*k + label_i;
+      if(o2 < 0 || o2 >= nZ) {
+	fprintf(err, "o2 is out of range: o2 = %ld, nZ = %ld\n", o2, nZ);
+	fflush(err);
+	fatal("assertion failed");
+      }
+      Z[o2] += *X2/(double)nk[label_i];
+    }
   }
 
-  normalize(Z, n, k);
+  // normalize(Z, n, k);
+
+  fprintf(err, "about to output\n");
+  fflush(err);
 
   if(fwrite(Z, sizeof(float), n*k, stdout) != n*k)
     fatal("write failed");
+
+  return 0;
 }
--- src/GEE.py
+++ src/GEE.py
@@ -1,49 +1,36 @@
 #!/usr/bin/env python
 
-import sys,os,argparse,scipy,psutil
+import sys,os,argparse,scipy,psutil,subprocess
 import numpy as np
-import copy
-from numpy import linalg as LA
 from sklearn.metrics.cluster import adjusted_rand_score
 import faiss
 from sklearn import metrics
 import time
-# node2vec
-# from node2vec import Node2Vec
-import networkx as nx
-# for sparse matrix
-from scipy import sparse
-#early stop
-from keras.callbacks import EarlyStopping
-from tensorflow.keras.callbacks import ModelCheckpoint
+
+
 
 print('GraphEncoder_clustering: ' + str(sys.argv), file=sys.stderr)
+sys.stderr.flush()
 
 GEEsrc=os.environ.get('GEEsrc')
 
 kwc_save_offset=0
 
-t0 = time.time()                # added by kwc
-
-# import numpy as np
-# import argparse
-# from sklearn.metrics.pairwise import cosine_similarity
-
-# apikey=os.environ.get('SPECTER_API_KEY')
+t0 = time.time()
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-O", "--output", help="output file", required=True)
+# parser.add_argument("-O", "--output", help="output file", required=True)
 parser.add_argument("--save_prefix", help="output file", default=None)
-parser.add_argument("-G", "--input_graph", help="input graph (readable by scipy.sparse.load_npz)", default=None)
+parser.add_argument("-G", "--input_graph", help="input graph (pathname minus .X.i)", default=None)
 parser.add_argument("-d", "--input_directory", help="input directory with embedding", default=None)
-parser.add_argument("-K", "--n_components", type=int, help="hidden dimensions [defaults = 32]", default=32)
+# parser.add_argument("-K", "--n_components", type=int, help="hidden dimensions [defaults = 32]", default=32)
 parser.add_argument("--Laplacian", type=int, help="Laplacian [defaults = 1 (True)]", default=1)
 parser.add_argument("--MaxIter", type=int, help="MaxIter [defaults = 50]", default=50)
 parser.add_argument("--safe_mode", type=int, help="set to nonzero to be super careful", default=0)
 args = parser.parse_args()
 
 # Supress/hide the warning
-# invalide devide resutls will be nan
+# invalid results from division will be nan
 np.seterr(divide='ignore', invalid='ignore')
 
 def map_int64(fn):
@@ -100,12 +87,16 @@ def directory_to_config(dir):
            'map64' : map64_from_dir(dir),
            'embedding' : embedding_from_dir(dir, K)}
 
-def read_graph(fn):
+def read_graph(fn, old_to_new):
   if fn is None: return
-  return { 'X0' : map_int32(fn),
-           'X1' : map_int32(fn),
-           'X2' : map_float32(fn),
-           }
+  G =  { 'X0' : old_to_new[map_int32(fn + '.X.i')],
+         'X1' : old_to_new[map_int32(fn + '.Y.i')]}
+  X2path = fn + '.W.f'
+  if not os.path.exists(X2path):
+    G['X2'] = np.ones(len(G['X0']), dtype=np.float32)
+  else:
+    G['X2'] = map_float32(X2path)
+  return G
 
 kwc_save_offset=0
 
@@ -141,7 +132,7 @@ def save_X(G):
 #    The elements in X0, X1 specify the edges E
 #    X2 specifies the weights for each edge
 
-def create_Z(G, Y, n):
+def create_Z(G, Y, Zprev_path):
   """
   X0, X1, X2 specifies the graph: (V, E)
   all three vectors have the same length: s = |E|;
@@ -149,6 +140,7 @@ def create_Z(G, Y, n):
   X2 specifies the weights for each edge
   """
 
+  n = len(Y)
   global kwc_save_offset
 
   kwc_gee_t0 = time.time()          # added by kwc
@@ -156,51 +148,65 @@ def create_Z(G, Y, n):
   X0path,X1path,X2path = save_X(G)
 
   # to simplify things, lets avoid more complicated cases
-  if Y.shape[1] != 1: return None
+  # if Y.shape[1] != 1: return None
 
   YY = Y.reshape(-1).astype(np.int32)
   Ypath = args.save_prefix + '.Y.%d.i' % kwc_save_offset
   Zpath = args.save_prefix + '.Z_kwc.%d.f' % kwc_save_offset
   YY.tofile(Ypath)
 
-  cmd = '%s/C/GEE %s %s %s %s > %s' % (JSALTsrc, X0path, X1path, X2path, Ypath, Zpath)
-  print(cmd, file=sys.stderr)
-  os.system(cmd)
+  cmd = GEEsrc + '/C/GEE ' + ' '.join([X0path, X1path, X2path, Ypath, Zprev_path, Zpath + '.err'])
+  print('cmd: ' + cmd, file=sys.stderr)
+  # os.system(cmd)
+  with open(Zpath, 'wb') as outf:
+      result = subprocess.run(cmd, stdout=outf, shell=True)
+  print('result: [return code: %d]' % (result.returncode), file=sys.stderr)
   assert os.path.exists(Zpath), 'cmd failed'    
-  Z = np.memmap(Zpath, dtype=np.float32, shape=(n, k), mode='r')
+  # Z = np.memmap(Zpath, dtype=np.float32, shape=(n, k), mode='r')
+  Z = map_float32(Zpath).reshape(n, -1)
   print('gee kwc (plus time for saving): %0.3f; time so far: %0.3f; memory = %0.2f GBs' % (time.time() - kwc_gee_t0, time.time() - t0, psutil.Process().memory_info().rss / 1e9), file=sys.stderr) # added by kwc
   print(psutil.virtual_memory(), file=sys.stderr)
   sys.stderr.flush()
-  return Z
+  return Z,Zpath
 
 def faiss_kmeans(Z, K, max_iter):
   kmeans = faiss.Kmeans(d=Z.shape[1], k=K, niter=max_iter)
   kmeans.train(Z)
-  dist, labels = kmeans.index.search(queries, 1)
-  labels = labels.reshape(-1)
-  return labels
+  print('%0.3f sec: faiss_kmeans, finished training' % (time.time() - t0), file=sys.stderr)
+  dist, labels = kmeans.index.search(Z, 1)
+  return labels.reshape(-1)
 
-def create_Y(Z):
-  kmeans_t0 = time.time()
-  K = args.n_components
-  max_iter = args.MaxIter
-  # kmeans = MiniBatchKMeans(n_clusters=K, max_iter = max_iter).fit(Z)
-  # labels = kmeans.labels_ # shape(n,)
-  labels = faiss_kmeans(Z, K, max_iter)
-  printf('%0.3f sec: kmeans' % (time.time() - kmeans_t0))
-  print(psutil.virtual_memory(), file=sys.stderr)
-  sys.stderr.flush()
-  return labels
+def create_Y(Z, K):
+    # K = args.n_components
+    max_iter = args.MaxIter
+    # kmeans = MiniBatchKMeans(n_clusters=K, max_iter = max_iter).fit(Z)
+    # labels = kmeans.labels_ # shape(n,)
+    labels = faiss_kmeans(Z, K, max_iter)
+    return labels
 
 assert args.input_directory or args.graph, 'need to specify --input_directory or --input_graph'
  
 config = directory_to_config(args.input_directory)
-G = read_graph(args.input_graph)
+G = read_graph(args.input_graph, config['map32'])
 
 if not config is None:
   Z1 = config['embedding']
-  Y1 = create_Y(Z)
-  Z2 = create_Z(Y)
+  Zpath = args.input_directory + '/embedding.f'
+
+for iteration in range(4):
+  Y1 = create_Y(Z1, Z1.shape[1])
+
+  print('%0.3f sec: Y1 computed, iteration: %d' % (time.time() - t0, iteration), file=sys.stderr)
+  print(psutil.virtual_memory(), file=sys.stderr)
+  sys.stderr.flush()
+
+  Z2,newZpath = create_Z(G, Y1, Zpath)
+
+  print('%0.3f sec: Z2 computed, iteration: %d' % (time.time() - t0, iteration), file=sys.stderr)
+  print(psutil.virtual_memory(), file=sys.stderr)
+  sys.stderr.flush()
+
+  Z1 = Z2
 
 
 print('done, %0.3f sec' % (time.time() - t0), file=sys.stderr)  # added by kwc
